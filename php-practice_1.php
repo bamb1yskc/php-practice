1f603633b385b7c69a70f80365296e4bd9b11a6a
犬塚悠介 <zucca@inudzukayuusukenoMacBook-Pro.local>
2026-03-23 01:09:40 +0900

    課題戻り修正

--- php-practice_1.php
+++ php-practice_1.php
@@ -18,14 +18,10 @@ echo '現在時刻は、 ' . $time . ' です。';
 // Q4 条件分岐-1 if文
 <?php
 $device = 'windows';
-if ($device === 'windows') {
-    echo '使用OSは、windowsです。';
+if ($device === 'windows' || $device === 'mac') {
+    echo '使用OSは、' . $device . 'です。';
 } else {
-    if ($device === 'mac') {
-        echo '使用OSは、macです。';
-    } else {
-        echo 'どちらでもありません。';
-    }
+    echo 'どちらでもありません。';
 }
 
 // Q5 条件分岐-2 三項演算子
